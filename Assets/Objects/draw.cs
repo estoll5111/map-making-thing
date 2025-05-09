1d35040cd408d10ed35ec6aa2e33b6e392fd72fb
Elinor <158220019+estoll5111@users.noreply.github.com>
2025-05-09 09:56:34 -0700

    idk

--- Assets/Objects/draw.cs
+++ Assets/Objects/draw.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class draw : MonoBehaviour
 {
+
+    public GameObject drawSphere; 
+    private Vector3 mousePos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+        Ray ray = new Ray(Camera.main.ScreenPointToRay(mousePos).origin, Camera.main.ScreenPointToRay(mousePos).direction);
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity)){
+            GameObject drawn = Instantiate(drawSphere, hit.point, Quaternion.identity);
+        }
     }
 }
--- Assets/Objects/drawTest.cs
+++ Assets/Objects/drawTest.cs
@@ -15,36 +15,7 @@ public class drawTest : MonoBehaviour
     public KeyCode draw = KeyCode.Mouse0;
     private void Awake()
     {
-        Mesh testMesh = new Mesh();
-
-        Vector3[] vertices = new Vector3[4];
-        Vector2[] uv = new Vector2[4];
-        int[] triangles = new int[6];
-
-        vertices[0] = new Vector3(-0.1f, 0.1f);
-        vertices[1] = new Vector3(-0.1f, -0.1f);
-        vertices[2] = new Vector3(0.1f, -0.1f);
-        vertices[3] = new Vector3(0.1f, 0.1f);
-
-        uv[0] = Vector2.zero;
-        uv[1] = Vector2.zero;
-        uv[2] = Vector2.zero;
-        uv[3] = Vector2.zero;
-
-        triangles[0] = 0;
-        triangles[1] = 3;
-        triangles[2] = 1;
-
-        triangles[3] = 1;
-        triangles[4] = 3;
-        triangles[5] = 2;
-
-        testMesh.vertices = vertices;
-        testMesh.uv = uv;
-        testMesh.triangles = triangles;
-        testMesh.MarkDynamic();
-        GetComponent<MeshFilter>().mesh = testMesh;
-
+    
         mousePos = Input.mousePosition;
         mousePos.z = Camera.main.nearClipPlane;
         Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(mousePos);
